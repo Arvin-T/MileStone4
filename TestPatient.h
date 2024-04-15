70bf0571ed1d4cff49adc8bd2d106b304c80fe15
Arvin <136032811+Arvin-T@users.noreply.github.com>
2024-04-14 23:22:29 -0400

    Add files via upload

---
+++ TestPatient.h
@@ -0,0 +1,32 @@
+/***********************************************************************
+Milestone 4
+Name:         Seyedarvin Tabatabaei Ferezghi
+Seneca email: stabatabaei-ferezghi@myseneca.ca
+Student ID:   122267230
+Date:         APL 14 2024
+***********************************************************************/
+#ifndef TEST_PATIENT_H
+#define TEST_PATIENT_H
+
+#include "Patient.h"
+
+namespace seneca
+{
+
+    class TestPatient : public Patient
+    {
+    private:
+        static int nextTestTicket;
+
+    public:
+        TestPatient();
+        char type() const override;
+        std::ostream &write(std::ostream &os) const override;
+        std::istream &read(std::istream &is) override;
+
+        // No explicit destructor needed for now
+    };
+
+} // namespace seneca
+
+#endif // TEST_PATIENT_H
---
+++ Ticket.h
@@ -0,0 +1,30 @@
+/***********************************************************************
+Milestone 4
+Name:         Seyedarvin Tabatabaei Ferezghi
+Seneca email: stabatabaei-ferezghi@myseneca.ca
+Student ID:   122267230
+Date:         APL 14 2024
+***********************************************************************/
+#ifndef SENECA_TICKET_H_
+#define SENECA_TICKET_H_
+#include <iostream>
+
+#include "Time.h"
+#include "IOAble.h"
+namespace seneca
+{
+    class Ticket : public IOAble
+    {
+        Time m_time;
+        int m_number;
+
+    public:
+        Ticket(int);
+        Time time() const;
+        int number() const;
+        void resetTime();
+        std::ostream &write(std::ostream &) const;
+        std::istream &read(std::istream &);
+    };
+}
+#endif
